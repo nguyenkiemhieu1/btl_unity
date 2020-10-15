c158b5ba9420dccfd3f48bf169125b8dc0c63bba
nguyenkiemhieu1 <hieunguyen18021999@gmail.com>
2020-10-15 19:58:32 +0700

    baiTuTom

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Door : MonoBehaviour
+{
+    public int Levelload = 1;
+    public gamemaster gm;
+
+    // Use this for initialization
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("gamemaster").GetComponent<gamemaster>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            //savescore();
+            gm.Inputtext.text = ("Press E to enter");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                
+        //int currentLevel = SceneManager.GetActiveScene().buildIndex;
+
+        //if (currentLevel >= PlayerPrefs.GetInt("levelIsUnlocked"))
+        //{
+        //    PlayerPrefs.SetInt("levelIsUnlocked", currentLevel + 1);
+        //}
+
+        //Debug.Log("LEVEL" + PlayerPrefs.GetInt("levelIsUnlocked") + " UNLOCKED");
+
+
+                //savescore();
+                SceneManager.LoadScene(Levelload);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            gm.Inputtext.text = ("");
+        }
+    }
+
+    //void savescore()
+    //{
+    //    PlayerPrefs.SetInt("points", gm.points);
+    //}
+}
---
+++ Assets/Scripts/Door1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door1 : MonoBehaviour
+{
+    public int Levelload1 = 3;
+    public gamemaster1 gm1;
+
+    // Use this for initialization
+    void Start()
+    {
+        gm1 = GameObject.FindGameObjectWithTag("gamemaster1").GetComponent<gamemaster1>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            //savescore();
+            gm1.Inputtext.text = ("Press E to enter");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                //savescore();
+                SceneManager.LoadScene(Levelload1);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            gm1.Inputtext.text = ("");
+        }
+    }
+
+    //void savescore()
+    //{
+    //    PlayerPrefs.SetInt("points", gm.points);
+    //}
+}
